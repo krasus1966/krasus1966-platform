0ccb07816dda4f7054f0bb23a419c192bc8e7109
krasus1966 <2134123985@qq.com>
2022-03-28 21:04:18 +0800

    提交

--- krasus1966-tools/krasus1966-tools-login/src/main/java/top/krasus1966/login/config/ArgumentResolverConfiguration.java
+++ krasus1966-tools/krasus1966-tools-login/src/main/java/top/krasus1966/login/config/ArgumentResolverConfiguration.java
@@ -1,29 +1,29 @@
-package top.krasus1966.login.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
-
-/**
- * @author Krasus1966
- * @date 2021/9/27 23:03
- **/
-@Configuration
-public class ArgumentResolverConfiguration implements WebMvcConfigurer {
-
-    public CurrentUserLoginInfoResolver getCurrentUserLoginInfoResolver() {
-        return new CurrentUserLoginInfoResolver();
-    }
-
-    /**
-     * 注册自定义参数解析器
-     *
-     * @param resolvers
-     */
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(getCurrentUserLoginInfoResolver());
-    }
-}
+//package top.krasus1966.login.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import java.util.List;
+//
+///**
+// * @author Krasus1966
+// * @date 2021/9/27 23:03
+// **/
+//@Configuration
+//public class ArgumentResolverConfiguration implements WebMvcConfigurer {
+//
+//    public CurrentUserLoginInfoResolver getCurrentUserLoginInfoResolver() {
+//        return new CurrentUserLoginInfoResolver();
+//    }
+//
+//    /**
+//     * 注册自定义参数解析器
+//     *
+//     * @param resolvers
+//     */
+//    @Override
+//    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+//        resolvers.add(getCurrentUserLoginInfoResolver());
+//    }
+//}
